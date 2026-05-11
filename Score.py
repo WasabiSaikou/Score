3def5f76a8f381039dabfd7ecce10e85c2fcc470
WasabiSaikou <65612399+WasabiSaikou@users.noreply.github.com>
2026-05-11 10:20:08 +0800

    fixed output and init'd total

--- Score.py
+++ Score.py
@@ -2,6 +2,7 @@ grades = list(map(int, input().split()))
 fail = 0
 max = 0
 min = 100
+total = 0
 for grade in grades:
     if grade < 60:
         fail += 1
@@ -15,5 +16,7 @@ for grade in grades:
 average = total / len(grades)
 
 print(average)
-print("不及格人數:", fail)
-print(f"max: {max}, min: {min}")
+print(f"不及格人數:{fail}")
+print(f"最大值:{max}")
+print(f"最小值:{min}")
+print(f"平均值:{average}")
